4768c0958f99092f35b9798207e7b88dba811b70
Ido <benhaim.ido@gmail.com>
2021-02-17 13:31:22 -0500

    Update OnLeave.cs

--- src/TheGPvp/Events/OnLeave.cs
+++ src/TheGPvp/Events/OnLeave.cs
@@ -7,15 +7,26 @@ namespace TheGPvp.Events
 {
     public class OnLeave : IScript
     {
-        [Target(GameSourceEvent.PlayerDestroy, ExecutionMode.Event)]
-        public void OnEvent(ShPlayer player)
+        [Target(GameSourceEvent.PlayerDestroy, ExecutionMode.Test)]
+        public bool TestEvent(ShPlayer player)
         {
             if (!player.isHuman || player.GetExtendedPlayerPvp().ActivePvp == null)
             {
-                return;
+                return false;
             }
             var arena = player.GetExtendedPlayerPvp().ActivePvp;
             arena.DisconnectPlayer(player);
+            Core.Instance.PlayerHandler.Remove(player.ID);
+            return false;
+        }
+
+        [Target(GameSourceEvent.PlayerDestroy, ExecutionMode.PostEvent)]
+        public void PostEvent(ShPlayer player)
+        {
+            if (!player.isHuman)
+            {
+                return;
+            }
             Core.Instance.PlayerHandler.Remove(player.ID);
         }
     }
